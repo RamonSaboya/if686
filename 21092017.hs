16deabcd151be3fb73a090aa5773750bbeb3b5c2
Ramon Saboya <ramons.gomes@hotmail.com>
2017-09-26 16:50:24 -0300

    Update 21092017.hs

--- 21092017.hs
+++ 21092017.hs
@@ -1,12 +1,7 @@
 mdc x y | (mod x y) == 0 = y
         | otherwise = mdc y (mod x y)
 
-valid :: Integer -> Integer -> Bool
-valid x n | x < n = False
-          | n == 2 = (mod x 2 == 0)
-          | otherwise = (valid x (n - 1)) || (mod x n == 0)
-
-test x n = valid x (floor(sqrt(n)))
+divisible x y = mod x y /= 0
 
 crivo :: Integer -> [Integer]
-crivo n = [x | x <- [2..n], (test x n)]
+crivo n = [x | x <- [2..n], all (divisible x)[2..(floor(sqrt(fromIntegral x)))]]
