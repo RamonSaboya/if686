82ccf5aac70aecb5d0323143e00322def47d7092
RamonSaboya <ramons.gomes@hotmail.com>
2017-09-27 19:31:02 -0300

    Starting second exercise
    Signed-off-by: RamonSaboya <ramons.gomes@hotmail.com>

--- 26092017.hs
+++ 26092017.hs
@@ -1,2 +1,15 @@
-fat n | n == 1 = [1]
-      | otherwise = (fat (n - 1)) ++ [n * (last (fat (n - 1)))]
+-- ==================================
+fat :: Int -> [Int]
+fat 1 = [1]
+fat n = (fat (n - 1)) ++ [n * (last (fat (n - 1)))]
+
+-- ==================================
+testaListaRecursao :: (a -> Bool) -> [a] -> Bool
+testaListaRecursao _ [] = True
+testaListaRecursao f (x:xs) = (f x) && (testaListaRecursao f xs)
+
+testaListaMap :: (a -> Bool) -> [a] -> Bool
+testaListaMap f l = and(map f l)
+
+testaListaFold :: (a -> Bool) -> [a] -> Bool
+testaListaFold f l = foldr (\a b -> f a && b) True l 
